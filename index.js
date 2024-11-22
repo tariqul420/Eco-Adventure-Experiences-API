f3c854d4f55912fac789fcca58de29ccf52dc07f
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-11-23 00:20:50 +0600

    Eco-Adventure-Experiences-API

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const adventures = require("./json/adventures.json");
+const featuredAdventures = require("./json/featuredAdventures.json");
+const testimonials = require("./json/testimonials.json");
+const app = express();
+const port = 3000;
+
+app.get("/adventures", (req, res) => {
+    res.send(adventures);
+});
+
+app.get("/adventures/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const adventure = adventures.find((adventure) => adventure.id === id);
+    res.send(adventure);
+});
+
+app.get("/featuredAdventures", (req, res) => {
+    res.send(featuredAdventures);
+});
+
+app.get("/testimonials", (req, res) => {
+    res.send(testimonials);
+});
+
+app.listen(port, () => {
+    console.log(`🌟 My listening port ${port}`);
+});
